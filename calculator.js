788cf91ca43f4452de7811fdb109cbea2ba19950
raych2 <rtongco@gmail.com>
2020-11-08 19:52:01 -0800

    Added new classes and ids to button elements and p element to display-box. Added DOM elements and event listeners to display the number clicked and to clear the number from the display. Modified css due to new classes

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
+let numbers = document.querySelectorAll('.numbers');
+let operators = document.querySelectorAll('.operators');
+let displayValue = document.querySelector('.current-nums');
+let addition = document.getElementById('add');
+let subtraction = document.getElementById('subtract');
+let multiplication = document.getElementById('multiply');
+let division = document.getElementById('divide');
+let equals = document.getElementById('equals');
+let allClear = document.getElementById('ac');
+let clear = document.getElementById('clear');
+let firstNum;
+let secondNum;
+
+displayValue.innerText = 0;
+
 //math operators
 function add (x,y) {
     return x + y;
@@ -17,11 +32,11 @@ function divide (x,y) {
 
 //operate
 function operate(operator,x,y) {
-    if (operator === 'add') {
+    if (operator === addition) {
         return add(x,y);
-    } else if (operator === 'subtract') {
+    } else if (operator === subtraction) {
         return subtract(x,y);
-    } else if (operator === 'multiply') {
+    } else if (operator === multiplication) {
         return multiply(x,y);
     } else {
         return divide(x,y);
@@ -29,3 +44,19 @@ function operate(operator,x,y) {
 }
 
 operate();
+
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function(e) {
+        displayValue.innerText = 0;
+        displayValue.innerText = this.textContent;
+        firstNum = displayValue.innerText;
+        console.log(firstNum);
+    });
+}
+
+allClear.addEventListener('click', function(e) {
+    displayValue.innerText = 0;
+    firstNum = displayValue.innerText;
+    console.log(firstNum);
+})
