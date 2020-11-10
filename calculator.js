4086b98838f910a9952cf12d5f0224e8a974bf99
raych2 <rtongco@gmail.com>
2020-11-09 23:13:34 -0800

    Deleted unnecessary DOM elements. Updated initial variables and event listeners for number, operator, equal, and all clear buttons

--- calculator.js
+++ calculator.js
@@ -1,17 +1,16 @@
 let numbers = document.querySelectorAll('.numbers');
 let operators = document.querySelectorAll('.operators');
 let displayValue = document.querySelector('.current-nums');
-let addition = document.getElementById('add');
-let subtraction = document.getElementById('subtract');
-let multiplication = document.getElementById('multiply');
-let division = document.getElementById('divide');
 let equals = document.getElementById('equals');
 let allClear = document.getElementById('ac');
 let clear = document.getElementById('clear');
-let firstNum;
-let secondNum;
 
-displayValue.innerText = 0;
+let currentNum = '';
+let previousNum = '';
+let operator = '';
+let totalValue = 0;
+
+displayValue.innerText = '';
 
 //math operators
 function add (x,y) {
@@ -32,31 +31,54 @@ function divide (x,y) {
 
 //operate
 function operate(operator,x,y) {
-    if (operator === addition) {
+    if (operator === '+') {
         return add(x,y);
-    } else if (operator === subtraction) {
+    } else if (operator === '-') {
         return subtract(x,y);
-    } else if (operator === multiplication) {
+    } else if (operator === '*') {
         return multiply(x,y);
     } else {
         return divide(x,y);
     }
 }
 
-operate();
-
-
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', function(e) {
-        displayValue.innerText = 0;
-        displayValue.innerText = this.textContent;
-        firstNum = displayValue.innerText;
-        console.log(firstNum);
+numbers.forEach(num => {
+    num.addEventListener('click', function(e) {
+        if (operator === '') {
+            displayValue.innerText += num.textContent;
+            currentNum = +(displayValue.innerText);
+        } else if (previousNum) {
+            displayValue.innerText += num.textContent;
+            currentNum = +(displayValue.innerText);
+        }
     });
-}
+});
+
+operators.forEach(op => {
+    op.addEventListener('click', function(e) {
+        if (op.id !== 'equals') {
+            operator = op.textContent;
+            previousNum = currentNum;
+            currentNum = '';
+            displayValue.innerText = '';
+        }
+    })
+});
+
+equals.addEventListener('click', function(e) {
+    if (operator === '/' && currentNum === 0) {
+        displayValue.innerText = 'Nice try!';
+        return;
+    }
+    totalValue = operate(operator, previousNum, currentNum);
+    displayValue.innerText = totalValue;
+    currentNum = totalValue;
+    operator = '';
+});
 
 allClear.addEventListener('click', function(e) {
-    displayValue.innerText = 0;
-    firstNum = displayValue.innerText;
-    console.log(firstNum);
-})
+    currentNum = '';
+    previousNum = '';
+    operator = '';
+    displayValue.innerText = '';
+});
